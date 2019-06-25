611d462cb94e8dd1b125f8596e7c14dbb638d6ed
Amandine Turpin <amandyne.turpin@gmail.com>
2019-06-25 21:20:37 +0200

    [2506][add] 18 - Validation

--- blog/src/Entity/Tag.php
+++ blog/src/Entity/Tag.php
@@ -5,9 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TagRepository")
+ * @UniqueEntity("name")
  */
 class Tag
 {
@@ -19,7 +22,9 @@ class Tag
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank()
+     * @Assert\Length(max="255")
      */
     private $name;
 
